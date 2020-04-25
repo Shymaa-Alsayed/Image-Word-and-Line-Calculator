c240473eea4627bb8a5d2fce3cdcba8e2b4b6db1
Shymaa-Alsayed <shymaa.alsayed49@gmail.com>
2020-04-26 01:02:23 +0200

    modified calculation of number of words using gamma distribution

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
-
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import scipy.stats as stats
 def process_image(img):
     #low = cv2.pyrDown(img)
     #lower = cv2.pyrDown(low)
@@ -24,10 +27,15 @@ def crop_word(img,word_start_and_end):
     return word_img
 
 def word_segmentation(line_img):
-    word_start_and_end = projection_histogram(line_img, type='horizontal',dil=True)
-    return  word_start_and_end
+    no_words,line_img = calculate_no_words_in_line(line_img)
+    word_start_and_end=[]
+    for i in range(1,16,2):
+        for j in range(1,16,2):
+            word_start_and_end = projection_histogram(line_img, type='horizontal',dil=True,window=(i,j))
+            if (no_words==len(word_start_and_end))or (no_words==len(word_start_and_end)+1) or (no_words==len(word_start_and_end)-1):
+                return word_start_and_end
 
-def projection_histogram(img,type,dil):
+def projection_histogram(img,type,dil,window=(3,3)):
     thresh=process_image(img)
     if type=='vertical':
         vertical_histogram = thresh.sum(axis=1)
@@ -44,7 +52,7 @@ def projection_histogram(img,type,dil):
 
     if type=='horizontal':
         if dil==True:
-            kernel = np.ones((5, 5), np.uint8)
+            kernel = np.ones(window, np.uint8)
             dilated = cv2.erode(img, kernel, iterations=1)  ##because ot is a white bckground, black letters
             ret, thresh = cv2.threshold(dilated, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
@@ -99,21 +107,91 @@ def calculate_spaces(line_img):
            else:
                spaces.append(count)
                count=0#
+    print(sorted(spaces))
     return spaces
 
-def run(img_name):
-    img=cv2.imread(img_name,0)
-    lines_coord=line_segmentation(img)
-    total_spaces=[]
-    for i in range(len(lines_coord)):
-        line_img=crop_line(img,lines_coord[i])
-        spaces=calculate_spaces(line_img)
-        total_spaces.extend(spaces)
-    mean_space=np.mean(total_spaces)
-    std_space=np.std(total_spaces)
-    no_words_plus_one = [x for x in total_spaces if x > mean_space + std_space]
-    no_words = len(no_words_plus_one) - 1
-    return  no_words,total_spaces
 
-img_name='btest.PNG'
-characters=character_segmentation(img_name)
+def calculate_no_words_in_line(line_img):
+    spaces=calculate_spaces(line_img)
+    alpha,loc,beta=stats.gamma.fit(spaces)
+    gamma_mean=(alpha/beta)
+    hist_=np.histogram(spaces)
+    cutoff_length=np.floor(len(hist_[1])*gamma_mean)
+    cutoff_value=hist_[1][int(cutoff_length)]
+    word_spaces=[ value for value in spaces if value>cutoff_value]
+    no_words=len(word_spaces)
+    return  no_words,line_img
+
+
+
+class Root(Tk):
+    img_name=''
+    def __init__(self):
+        super(Root, self).__init__()
+        self.title("Word and Line Calculator")
+        self.minsize(500, 300)
+
+        self.labelFrame = ttk.LabelFrame(self, text = "Open File")
+        self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
+
+        self.line_label=ttk.Label( text = "Number of lines is ")
+        self.line_label.place(x = 20, y =155)
+
+        self.word_label=ttk.Label( text = "Number of words is ")
+        self.word_label.place(x = 20, y =175)
+
+        self.avg_label=ttk.Label( text = "Average number of words in a line is ")
+        self.avg_label.place(x = 20, y =195)
+
+        self.browse_button()
+        self.calculate_button()
+
+
+    def browse_button(self):
+        self.browse_button = ttk.Button(self.labelFrame, text = "Browse A File",command = self.fileDialog)
+        self.browse_button.grid(column = 1, row = 1)
+
+    def calculate_button(self):
+        self.calculate_button =ttk.Button(text='Calculate',command=self.run)
+        self.calculate_button.place(x= 20, y=120)
+
+    def fileDialog(self):
+        self.filename = filedialog.askopenfilename(initialdir =  "/", title = "Select A File", filetype =
+        (("jpeg files","*.jpg"),("all files","*.*")) )
+        self.img_name=self.filename
+        print(self.img_name)
+        self.label = ttk.Label(self.labelFrame, text = "")
+        self.label.grid(column = 1, row = 2)
+        self.label.configure(text = self.filename)
+
+    def run(self):
+        img = cv2.imread(self.img_name, 0)
+        lines_coord = line_segmentation(img)
+        total_no_of_lines = len(lines_coord)
+        total_no_of_words = 0
+        print(lines_coord)
+        for line in lines_coord:
+            line_img = crop_line(img, line)
+            word_start_and_end = word_segmentation(line_img)
+            print(word_start_and_end)
+            total_no_of_words += len(word_start_and_end)
+        avg_no_words_in_line = round(total_no_of_words / len(lines_coord),1)
+
+        self.line_label=ttk.Label( text = "")
+        self.line_label.place(x = 125, y = 155)
+        self.line_label.configure(text = total_no_of_lines)
+
+        self.word_label=ttk.Label( text = "")
+        self.word_label.place(x = 130, y = 175)
+        self.word_label.configure(text = total_no_of_words)
+
+        self.avg_label=ttk.Label( text = "")
+        self.avg_label.place(x = 220, y = 195)
+        self.avg_label.configure(text = avg_no_words_in_line)
+        #return total_no_of_lines, total_no_of_words, avg_no_words_in_line
+
+
+
+root = Root()
+root.mainloop()
+
